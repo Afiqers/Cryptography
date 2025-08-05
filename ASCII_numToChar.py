13dd4ae220b78486768233ca06f72ba8ff876f76
Afiqers <afiqq2801@gmail.com>
2025-08-05 21:11:04 +0800

    Update ASCII_numToChar.py

--- ASCII_numToChar.py
+++ ASCII_numToChar.py
@@ -7,4 +7,6 @@ if sys.version_info.major == 2:
 ords = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
 print("Here is your flag:")
-print("".join(chr(o ^ 0x32) for o in ords))
+print("".join(chr(o) for o in ords))
+
+# In Python, the chr() function can be used to convert an ASCII ordinal number to a character (the ord() function does the opposite).
