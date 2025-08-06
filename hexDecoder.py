2d60be5a0628be90df2a57118c12a132f2a6357c
Afiqers <afiqq2801@gmail.com>
2025-08-06 22:33:23 +0800

    Hex Decoder

--- hexDecoder.py
+++ hexDecoder.py
@@ -14,6 +14,6 @@ def decode_hex_string(hex_string):
     except ValueError:
         return "Invalid hex string"
 
-# Declare the hex string here
+#Declare hex string here
 hex_input = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
-print("Decoded string:", decode_hex_string(hex_input))
+print(decode_hex_string(hex_input))
